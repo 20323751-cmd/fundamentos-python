45ed71965ac689b571648b89cf97c5418f447e08
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:07:13 -0600

    Create condicionales.py

---
+++ práctica 5/condicionales.py
@@ -0,0 +1,12 @@
+#Logico boolean booleano bool
+#verdadero / Falso: True / False
+#dato = 5 < 5 
+#< > ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "Alisson":
+    print("Bienvenida Alisson")
+else:
+    print("Que nombre tan extraño")
