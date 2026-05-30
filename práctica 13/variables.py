430d9ed576e814f984f28f9e2293c13c3d0e933c
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:24:14 -0600

    Create variables.py

---
+++ práctica 13/variables.py
@@ -0,0 +1,14 @@
+#soy un comentario
+#strings , cadenas ,textos
+#numericos , int (integrar, entero)float(flotante)
+nombre="Alisson"
+apellido="granados"
+edad="17"
+
+print(nombre, apellido, edad)
+
+#edad=edad/3
+
+print(nombre, apellido,edad)
+
+print(type(edad))
