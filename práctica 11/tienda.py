01d5bc63cc7be299c224ef2e711253b90f5a9765
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:20:19 -0600

    Create tienda.py

---
+++ práctica 11/tienda.py
@@ -0,0 +1,19 @@
+print("*************************")
+print("***     BIENVENIDO A    **")
+print("  LA TIENDA DE MAQUILLAJE DE ALISSON  **")
+print("*****************************")
+
+num_gloss=5
+num_pestañol=20
+num_labiales=7
+num_gancho=4
+num_polvos=15
+num_pelucas=30
+num_peines=2
+num_tintes=9
+
+Accesorios_totales=num_gloss + num_pestañol + num_labiales + num_gancho + num_polvos + num_pelucas + num_peines + num_tintes
+
+print("Actualmente contamos con")
+print("glos:",num_gloss ,"pestañol:" ,num_pestañol, "labiales:", num_labiales, "gancho:", num_gancho ,"polvos:", num_polvos, "pelucas:",num_pelucas,"peines:",num_peines,"peine:",)
+print("En total tenemos" ,Accesorios_totales,"Accesorios")
