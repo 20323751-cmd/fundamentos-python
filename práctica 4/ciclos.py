2aa1b1384a5c0e865af582a2f1c60df50e23f022
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:05:56 -0600

    Create ciclos.py

---
+++ práctica 4/ciclos.py
@@ -0,0 +1,31 @@
+   print("El numero", i, "es menor a 5")
+else:
+    print("El numero", i, " es mayor o igual a 5")
+
+    i + 1
+
+    print("Termino a iteración")
+
+
+    #for x  in range (1,11):
+
+
+
+
+#print(x)
+
+while True:  
+print("Escribe la opción deseada")
+print("1 saludar")
+print("2: salir")
+
+
+
+respuesta = int(inut())
+
+if respuestas == 1:
+    print("saludos terricola!")
+elif respuesta == 2:
+    break
+
+print("Terminando programa")
