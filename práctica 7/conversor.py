1679d23eccb015f79999e3bbe8985317786c86e2
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:12:24 -0600

    Create conversor.py

---
+++ práctica 7/conversor.py
@@ -0,0 +1,15 @@
+print("Bienvenido al conversor  de millas  a kilómetros")
+
+print("Escribe un numero  en millas :")
+millas = input()
+
+print("Tipo de datos  de millas ", type(millas))
+#convertit su atring a numero
+millas = float(millas)
+print("Tipo de datos de millas",type(millas))
+
+# 1 millas =  1.609 kms 
+kilometros = millas * 1.609
+
+print("Millas ingresadas:" , millas)
+print("Kilometros:, Kilometros)")
