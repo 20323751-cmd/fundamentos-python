4c3b6321491af1830e01ca82b4a419ebdd7d173e
20323751-cmd <20323751@clases.edu.sv>
2026-05-30 16:09:21 -0600

    Create condicionales2.py

---
+++ práctica 6/condicionales2.py
@@ -0,0 +1,16 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#elif
+#Operadores lógicos
+#and (y)
+
+
+if nombre == "Alisson":
+    print("saludo Alisson")
+    print("saludo Alisson")
+elif nombre == "Alisson":
+    print("Que bonito nombre Alisson ")
+elif nombre == "kerin"
+else:
+    print
